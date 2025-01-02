81749994d105ba134e81cf4a56405adf78e56698
Jun Ukemori <jukemori@gmail.com>
2025-01-02 19:45:45 +0900

    feat: use themeprovider to root layout for dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
-import { Navbar } from '@/components/Navbar'
+import { ThemeProvider } from '@/components/theme-provider'
+import { Navbar } from '@/components/navbar'
 import './globals.css'
 
 const poppins = Poppins({
@@ -20,10 +21,17 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={` ${poppins.variable} antialiased`}>
-        <Navbar />
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
