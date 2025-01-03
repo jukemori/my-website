33e7ee76f14d7b851fdf9e971fc5dd861ee9a1d5
Jun Ukemori <jukemori@gmail.com>
2025-01-03 09:46:23 +0900

    fix: switch buttons for dark mode

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -42,8 +42,8 @@ function ThemeToggle() {
       aria-label="Toggle theme"
     >
       <div className="relative">
-        <Sun className="h-5 w-5 rotate-0 scale-100 transition-all md:h-4 md:w-4 dark:-rotate-90 dark:scale-0" />
-        <Moon className="absolute left-0 top-0 h-5 w-5 rotate-90 scale-0 transition-all md:h-4 md:w-4 dark:rotate-0 dark:scale-100" />
+        <Sun className="h-5 w-5 rotate-90 scale-0 transition-all md:h-4 md:w-4 dark:rotate-0 dark:scale-100" />
+        <Moon className="absolute left-0 top-0 h-5 w-5 rotate-0 scale-100 transition-all md:h-4 md:w-4 dark:-rotate-90 dark:scale-0" />
       </div>
     </button>
   )
