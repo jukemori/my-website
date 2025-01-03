4b92441bf21d67f8244d5592fd5d4ef8d91ec5b4
Jun Ukemori <jukemori@gmail.com>
2025-01-03 17:17:05 +0900

    fix: improve iOS safe area bottom padding support

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -98,7 +98,7 @@ export function Navbar() {
         </Link>
 
         <div className="w-full md:w-auto md:border-0 md:bg-transparent md:p-0 md:pr-10">
-          <ul className="border-border safe-bottom fixed bottom-0 left-0 z-[100] grid w-full grid-cols-3 gap-8 border-t bg-background pb-4 pt-3 md:relative md:flex md:w-auto md:border-none md:bg-transparent md:p-0 md:pt-0">
+          <ul className="border-border safe-bottom fixed bottom-0 left-0 z-[100] grid w-full grid-cols-3 gap-8 border-t bg-background pt-3 md:relative md:flex md:w-auto md:border-none md:bg-transparent md:p-0 md:pt-0">
             {NAV_ITEMS.map((item) => (
               <NavLink key={item.href} {...item} />
             ))}
