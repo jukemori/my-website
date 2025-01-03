32f870d6031be4a3c6010d0ae30c61f3d8b72b78
Jun Ukemori <jukemori@gmail.com>
2025-01-03 22:23:25 +0900

    feat: create card for blog post

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,11 +6,14 @@ export default function BlogPage() {
   const posts = getBlogPosts()
 
   return (
-    <div className="py-16">
-      <h1 className="mb-8 text-3xl font-bold">Blog Posts</h1>
-      <ul className="space-y-8">
+    <>
+      <h1 className="mb-8 text-3xl font-bold md:text-4xl">Blog Posts</h1>
+      <ul className="grid grid-cols-1 place-items-center gap-8 md:grid-cols-2 md:place-items-start">
         {posts.map((post) => (
-          <li key={post.slug}>
+          <li
+            key={post.slug}
+            className="border-border w-full max-w-96 rounded-lg border shadow md:max-w-full"
+          >
             <Link
               href={`/blog/${post.slug}`}
               className="group block transition-opacity hover:opacity-90"
@@ -19,19 +22,25 @@ export default function BlogPage() {
                 <Image
                   src={post.image}
                   alt={post.title}
-                  className="mb-4 h-48 w-full rounded-lg object-cover"
-                  width={1000}
-                  height={1000}
+                  className="mb-4 h-48 rounded-t-lg object-cover md:h-60"
+                  width={700}
+                  height={700}
                 />
               )}
-              <h2 className="text-xl font-semibold">{post.title}</h2>
-              <time className="text-muted-text-sm">
-                {new Date(post.date).toLocaleDateString()}
-              </time>
+              <div className="p-4">
+                <h3 className="mb-2 text-xl font-semibold">{post.title}</h3>
+                <time className="text-muted text-sm">
+                  {new Date(post.date).toLocaleDateString('en-US', {
+                    month: 'short',
+                    day: 'numeric',
+                    year: 'numeric',
+                  })}
+                </time>
+              </div>
             </Link>
           </li>
         ))}
       </ul>
-    </div>
+    </>
   )
 }
