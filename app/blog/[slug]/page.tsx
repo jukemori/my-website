30439630028195b918a7085952d66df62588a61e
Jun Ukemori <jukemori@gmail.com>
2025-01-03 23:15:08 +0900

    feat: create single blog template

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import { getBlogPosts } from '@/lib/blog'
 import { MDXRemote } from 'next-mdx-remote/rsc'
 import { notFound } from 'next/navigation'
+import Image from 'next/image'
+import { formatDate } from '@/lib/blog'
 
 type Props = {
   params: Promise<{
@@ -25,12 +27,19 @@ export default async function BlogPost({ params }: Props) {
   }
 
   return (
-    <article className="mx-auto max-w-2xl py-16">
-      <h1 className="mb-4 text-3xl font-bold">{post.title}</h1>
-      <time className="text-muted mb-8 block text-sm">
-        {new Date(post.date).toLocaleDateString()}
+    <article className="mx-auto max-w-3xl">
+      <h1 className="mb-4 text-3xl font-bold md:text-4xl">{post.title}</h1>
+      <time className="text-muted mb-12 block text-sm">
+        {formatDate(post.date)}
       </time>
-      <div className="prose dark:prose-invert">
+      <Image
+        src={post.image}
+        alt={post.title}
+        className="mb-8 h-96 w-full rounded-lg object-cover"
+        width={700}
+        height={700}
+      />
+      <div className="prose dark:prose-invert prose-p:text-muted max-w-none">
         <MDXRemote source={post.content} />
       </div>
     </article>
