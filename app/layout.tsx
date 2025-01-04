f8ec4d16c7e99c6f1860775c6e70b404d5f61244
Jun Ukemori <jukemori@gmail.com>
2025-01-04 15:12:50 +0900

    feat: add style for devicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+        />
+      </head>
       <body className={`${poppins.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
