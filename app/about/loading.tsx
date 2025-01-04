c972756a1dde2711296d4e2bec0364df13bc5178
Jun Ukemori <jukemori@gmail.com>
2025-01-04 18:31:00 +0900

    feat: add about loading page

---
+++ app/about/loading.tsx
@@ -0,0 +1,34 @@
+import { Skeleton } from '@/components/ui/skeleton'
+
+export default function AboutPageLoading() {
+  return (
+    <div className="pb-24 md:pb-10">
+      <Skeleton className="mb-8 h-10 w-48" />
+
+      <section className="mb-8">
+        <Skeleton className="mb-5 h-7 w-32" />
+        <Skeleton className="mb-5 h-24" />
+        <Skeleton className="mb-5 h-24" />
+        <Skeleton className="mx-auto my-12 h-[250px] w-[350px] md:h-[350px] md:w-[590px]" />
+        <Skeleton className="mb-5 h-24" />
+        <Skeleton className="mb-5 h-24" />
+        <Skeleton className="mx-auto my-12 h-[250px] w-[350px] md:h-[350px] md:w-[590px]" />
+      </section>
+
+      <section className="mb-8">
+        <Skeleton className="mb-5 h-7 w-44" />
+        <Skeleton className="mb-5 h-24" />
+        <Skeleton className="mb-5 h-24" />
+        <Skeleton className="mb-5 h-24" />
+        <Skeleton className="mb-5 h-24" />
+        <Skeleton className="mx-auto my-12 h-[250px] w-[350px] md:h-[350px] md:w-[590px]" />
+      </section>
+
+      <section className="mb-8">
+        <Skeleton className="mb-5 h-7 w-28" />
+        <Skeleton className="mb-5 h-24" />
+        <Skeleton className="mb-5 h-16" />
+      </section>
+    </div>
+  )
+}
