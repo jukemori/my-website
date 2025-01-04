5389881e9180c17c642618a2a0d39f4635156a68
Jun Ukemori <jukemori@gmail.com>
2025-01-04 18:36:23 +0900

    feat: Add loading skeleton for blog list page

---
+++ app/blog/loading.tsx
@@ -0,0 +1,25 @@
+import { Skeleton } from '@/components/ui/skeleton'
+
+export default function BlogPageLoading() {
+  return (
+    <div className="pb-24 md:pb-10">
+      <h1 className="mb-8 text-3xl font-bold md:text-4xl">Blog Posts</h1>
+      <ul className="grid grid-cols-1 place-items-center gap-8 md:grid-cols-2 md:place-items-start">
+        {Array.from({ length: 4 }).map((_, i) => (
+          <li
+            key={i}
+            className="border-border w-full max-w-96 rounded-lg border shadow md:max-w-full"
+          >
+            <Skeleton className="h-48 rounded-t-lg md:h-60" />
+
+            <div className="p-4">
+              <Skeleton className="mb-2 h-6 w-3/4" />
+
+              <Skeleton className="h-4 w-1/4" />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
