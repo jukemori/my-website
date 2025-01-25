388189cd77f8fd789db7ad297fce3cb1a9672b76
Jun Ukemori <jukemori@gmail.com>
2025-01-25 10:01:08 +0900

    feat: add project page

---
+++ app/projects/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { ProjectCard } from '@/app/projects/_components/project-card'
+import projectData from '@/content/projects/projects.json'
+import { Project } from '@/app/projects/_components/project-card'
+
+const typedProjectData = projectData as Project[]
+
+const ProjectsPage = () => {
+  return (
+    <div className="pb-24 md:pb-10">
+      <h1 className="mb-8 text-3xl font-bold md:text-4xl">Projects</h1>
+      <div className="space-y-8">
+        {typedProjectData.map((project, index) => (
+          <ProjectCard key={index} project={project} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default ProjectsPage
